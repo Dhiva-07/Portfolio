4e9e5196b6bf64b02d4436eb31d1b4bc56e103f0
Dhiva-07 <dhiva21112004@gmail.com>
2025-01-19 20:02:31 +0530

    hero page started

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
-    <div className='container'>
-      <Hero/>
-      <Services/>
-      <Portfolio/>
-      <Contact/>
+    <div className="container">
+      <section id="#home">
+        <Hero />
+      </section>
+      <section id="#services">
+        <Services />
+      </section>
+      <section id="#portfolio">
+        <Portfolio />
+      </section>
+      <section id="#contact">
+        <Contact />
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
